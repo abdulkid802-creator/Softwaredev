5f8816a3a9db66ace15a2b8e0ddc420b17c431d7
a00022905 <a00022905@mytudublin.ie>
2026-03-05 12:44:21 +0000

    me

--- lab17.py
+++ lab17.py
@@ -1,40 +1,40 @@
-from statistics import mean
-
-hourly_rate = ([10.5, 12.0, 14.5, 16.75, 18.0], [20.5, 22.25, 24.0, 26.25, 28.0],
-               [34.0, 36.5, 38.0, 40.35, 43.0], [50.0, 60.0, 70.0, 80.0, 99.99],)
-
-for index, i in enumerate(hourly_rate):
-    average_pay = mean(i)
-    print(f"Average pay of Grade {index + 1} employees: €, {average_pay:.2f}")
-
-for index, i in enumerate(hourly_rate):
-    pay_difference = i[4] - i[0]
-    print(f"Pay difference at Grade Level {index + 1} €, {pay_difference:.2f}")
-
-counter = 1
-
-print(" " * 35, "Payscale Table")
-print(f"{"":15}{"Step 1":15}{"Step 2":15}{"Step 3":15}{"Step 4":15}{"Step 5":15}")
-print("-"* 90)
-
-for grade in hourly_rate:
-    print(f"Grade:{counter} {grade[0]:12}{grade[1]:15}{grade[2]:15}{grade[3]:15}{grade[4]:15}")
-    counter += 1
-
-print()
-
-
-INCREASE_PAY = 1.50
-counter = 1
-for row in range(len(hourly_rate)):
-    for col in range(len(hourly_rate[row])):
-        hourly_rate[row][col] += INCREASE_PAY
-
-
-print(" " * 35, "Payscale Updated Table")
-print(f"{"":15}{"Step 1":15}{"Step 2":15}{"Step 3":15}{"Step 4":15}{"Step 5":15}")
-print("-"* 90)
-
-for grade in hourly_rate:
-    print(f"Grade:{counter} {grade[0]:12}{grade[1]:15}{grade[2]:15}{grade[3]:15}{grade[4]:15}")
-    counter += 1
+# from statistics import mean
+#
+# hourly_rate = ([10.5, 12.0, 14.5, 16.75, 18.0], [20.5, 22.25, 24.0, 26.25, 28.0],
+#                [34.0, 36.5, 38.0, 40.35, 43.0], [50.0, 60.0, 70.0, 80.0, 99.99],)
+#
+# for index, i in enumerate(hourly_rate):
+#     average_pay = mean(i)
+#     print(f"Average pay of Grade {index + 1} employees: €, {average_pay:.2f}")
+#
+# for index, i in enumerate(hourly_rate):
+#     pay_difference = i[4] - i[0]
+#     print(f"Pay difference at Grade Level {index + 1} €, {pay_difference:.2f}")
+#
+# counter = 1
+#
+# print(" " * 35, "Payscale Table")
+# print(f"{"":15}{"Step 1":15}{"Step 2":15}{"Step 3":15}{"Step 4":15}{"Step 5":15}")
+# print("-"* 90)
+#
+# for grade in hourly_rate:
+#     print(f"Grade:{counter} {grade[0]:12}{grade[1]:15}{grade[2]:15}{grade[3]:15}{grade[4]:15}")
+#     counter += 1
+#
+# print()
+#
+#
+# INCREASE_PAY = 1.50
+# counter = 1
+# for row in range(len(hourly_rate)):
+#     for col in range(len(hourly_rate[row])):
+#         hourly_rate[row][col] += INCREASE_PAY
+#
+#
+# print(" " * 35, "Payscale Updated Table")
+# print(f"{"":15}{"Step 1":15}{"Step 2":15}{"Step 3":15}{"Step 4":15}{"Step 5":15}")
+# print("-"* 90)
+#
+# for grade in hourly_rate:
+#     print(f"Grade:{counter} {grade[0]:12}{grade[1]:15}{grade[2]:15}{grade[3]:15}{grade[4]:15}")
+#     counter += 1
--- week19.py
+++ week19.py
@@ -59,3 +59,69 @@
 # max_num, min_num, = min_max(nums)
 # print("The maximum value in the list : ",max_num)
 # print("The minimum value in the list : ",min_num)
+
+# def sum_row(list_in):
+#     totals = []
+#     for row in list_in:
+#         total = sum(row)
+#         totals.append(total)
+#     return totals
+#
+# nums = [[1,2,3,4], [5,6,7,8],[9,10,11,12]]
+# returned_list = sum_row(nums)
+# print(returned_list)
+# print(sum_row(nums))
+
+# def is_valid_code(code_in):
+#     valid_code = ["pt", "ft", "s"]
+#     if code_in.lower() in valid_code:
+#         return True
+#     else:
+#         return False
+# user_code = input("Worker's code: ")
+# user_valid = is_valid_code(user_code)
+#
+# while not user_valid:
+#     user_code = input("Invalid RE-Enter Worker's code: ")
+#     user_valid = is_valid_code(user_code)
+# print(user_valid, "is valid. Please continue. ")
+#
+# while not is_valid_code(user_code):
+#     user_code = input("Invalid RE-Enter Worker's code: ")
+# print(user_valid, "is valid. Please continue. ")
+
+# def apply_discount(rrp, discount_pc=0):
+#     sale_price = rrp *(1-(discount_pc/100))
+#     return sale_price
+# price = apply_discount(rrp=100, discount_pc= 50)
+# print(price)
+# price = apply_discount(100)
+# print(price)
+
+# def list_contains(list_in, value):
+#     occurrences = 0
+#     for item in list_in:
+#         if item == value:
+#             occurrences += 1
+#     if occurrences > 1:
+#         return True
+#     else:
+#         return False
+# my_list = [1, 2, 3, 4, 4, 9]
+# user_number = int(input("Enter Number: "))
+#
+# # in_list = list_contains(my_list, user_number)
+# # if in_list == True:
+#
+# if list_contains(my_list, user_number):
+#     print(user_number, "is in the list more than once")
+# else:
+#     print(user_number, "is not in the list more than once")
+
+# def determine2d_max(list_in):
+#     max_row = [max(row) for row in list_in]
+#     print(max_row)
+#     return max(max_row)
+# nums = [[1, 2, 3, 4],[5, 6, 7, 8], [9, 10, 11, 12]]
+# max_number = determine2d_max(nums)
+# print("The max value in the 2D list: ", max_number)
