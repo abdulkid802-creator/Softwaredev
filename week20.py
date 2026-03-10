f775807f738382e6ff22ceb375d59880f18c626c
a00022905 <a00022905@mytudublin.ie>
2026-03-10 12:45:17 +0000

    me

---
+++ week20.py
@@ -0,0 +1,25 @@
+# class car:
+#     def __init__(self):
+#         print("A new car as been created")
+#     def speak(self):
+#         print("I am an instance of the class car")
+#
+# mercedes = car()
+# bmw = car()
+# audi = car()
+# mercedes.speak()
+# bmw.speak()
+# audi.speak()
+
+# class Robot:
+#     def __init__(self, name_in, number_of_legs):
+#         self.name = name_in
+#         self.number_of_legs = number_of_legs
+#         print("I am alive")
+#
+# r1 = Robot(name_in= "Asimov", number_of_legs= 2)
+# print("Name: ", r1.name)
+# print("Legs: ", r1.number_of_legs)
+# r2 = Robot(name_in= "Capricorn", number_of_legs= 4)
+# print("Name: ", r2.name)
+# print("Legs: ", r2.number_of_legs)
