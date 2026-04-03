40a71963ccb7323edf295fe36d28d3c68fe75608
A00022905 <A00022905@mytudublin.ie>
2026-04-03 17:51:20 +0100

    me

--- lab22.py
+++ lab22.py
@@ -1,40 +1,40 @@
-# # import random
-# #
-# # class Animal:
-# #     def __init__(self, number_id, animal_type):
-# #         self.number = number_id
-# #         self.type = animal_type
-# #     def display_details(self):
-# #         print("ID", self.number)
-# #         print("Type", self.type)
-# #
-# # class Greyhound(Animal):
-# #     def __init__(self, name, sex, father, mother, litters, pup):
-# #         number_id = random.randint(1000, 9999)
-# #         super().__init__(number_id, "Greyhound")
-# #
-# #         self.name = name
-# #         self.sex = sex
-# #         self.father = father
-# #         self.mother = mother
-# #         self.litters = 0
-# #         self.pup = 0
-# #
-# #     def update_breeding_record(self, pup_born):
-# #         self.litters += 1
-# #         self.pup = pup_born
-# #
-# #     def display_details(self):
-# #         super().display_details()
-# #         print("Name", self.name)
-# #         print("Sex", self.sex)
-# #         print("Father", self.father)
-# #         print("Mother", self.mother)
-# #
-# # dog = Greyhound("Mrs Flash", "female", "Tom Foley", "The Late Late Show", 2, 9)
-# # dog.update_breeding_record(2)
-# # dog.update_breeding_record(4)
-# # dog.display_details()
+# import random
+#
+# class Animal:
+#     def __init__(self, number_id, animal_type):
+#         self.number = number_id
+#         self.type = animal_type
+#     def display_details(self):
+#         print("ID", self.number)
+#         print("Type", self.type)
+#
+# class Greyhound(Animal):
+#     def __init__(self, name, sex, father, mother, litters, pup):
+#         number_id = random.randint(1000, 9999)
+#         super().__init__(number_id, "Greyhound")
+#
+#         self.name = name
+#         self.sex = sex
+#         self.father = father
+#         self.mother = mother
+#         self.litters = 0
+#         self.pup = 0
+#
+#     def update_breeding_record(self, pup_born):
+#         self.litters += 1
+#         self.pup = pup_born
+#
+#     def display_details(self):
+#         super().display_details()
+#         print("Name", self.name)
+#         print("Sex", self.sex)
+#         print("Father", self.father)
+#         print("Mother", self.mother)
+#
+# dog = Greyhound("Mrs Flash", "female", "Tom Foley", "The Late Late Show", 2, 9)
+# dog.update_breeding_record(2)
+# dog.update_breeding_record(4)
+# dog.display_details()
 #
 # class Employee:
 #     def __init__(self, name = "", employee_number = 0, wages_per_hour = 0, hours_worked = 0):
